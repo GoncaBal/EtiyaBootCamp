9c1c44dbd8d2a5cb6ba7cf63e077b8b66b7651e1
Gonca Bal <goncabal94@gmail.com>
2022-07-21 00:38:53 +0300

    first day workshop

--- intro/src/intro/Main.java
+++ intro/src/intro/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// isPrime();
-		isVowelOrConsonant();
+//		isPrime();
+//		isVowelOrConsonant();
+//		isPerfectNumber();
+//		isFriendlyNumber();
+//		isNumberExist();
+		
 	}
 
 	static void isPrime() {
@@ -35,6 +39,64 @@ public class Main {
 			break;
 			default:
 				System.out.println("İnce sesli harf");
+		}
+	}
+	
+	static void isPerfectNumber() {
+		int number = 5;
+		int total=0;
+		
+		for(int i=1;i<number;i++) {
+			if(number%i==0) {
+			total=total+i;	
+			}
+		}
+		if(total==number) {
+			System.out.println("Mükemmel sayıdır.");
+		}else {
+			System.out.println("Mükemmel sayı değildir.");
+		}
+	}
+	
+	static void isFriendlyNumber() {
+		int sayi1=220;
+		int sayi2=284;
+		int toplam1=0;
+		int toplam2=0;
+		
+		for(int i=0;i<sayi1;i++) {
+			if(sayi1%i==0) {
+				toplam1=toplam1+i;
+			}
+		}
+		
+		for(int i=0;i<sayi2;i++) {
+			if(sayi2%i==0) {
+				toplam2=toplam2+i;
+			}
+		}
+		
+		if(sayi1==toplam2 && sayi2==toplam1) {
+			System.out.println("Bu iki sayı arkadaştır.");
+		}
+	}
+	
+	static void isNumberExist() {
+		int[]sayilar=new int[] {1,2,5,7,9,0};
+		int aranacak=5;
+		boolean varMi=false;
+		
+		for(int sayi : sayilar) {
+			if(sayi==aranacak) {
+				varMi=true;
+				break;
+			}
+		}
+		
+		if(varMi) {
+			System.out.println("Sayı mevcuttur.");
+		}else {
+			System.out.println("Sayı mevcut değildir.");
 		}
 	}
 }
