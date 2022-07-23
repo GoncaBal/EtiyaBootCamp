eb06c5662aeaa49ae5e4806eaffaf761e4daa1e3
Gonca <goncabal94@gmail.com>
2022-07-23 14:57:19 +0300

    updated

--- classesWithAttributes/src/ProductManager.java
+++ classesWithAttributes/src/ProductManager.java
@@ -1,8 +1,10 @@
 public class ProductManager {
-    public void add(Product product){
-        System.out.println("Ürün eklendi : "+ product.getName());
+    public void add(Product product) {
+        System.out.println("Ürün eklendi : " + product.getName());
     }
-    public void update(Product product){
-        System.out.println("Ürün güncellendi : "+product.getName());
+
+
+    public void update(Product product) {
+        System.out.println("Ürün güncellendi : " + product.getName());
     }
 }
